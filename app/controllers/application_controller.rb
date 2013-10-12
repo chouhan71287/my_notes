b66b7792749f115766a203a6feddfec63a15f566
Vijay <vijaychouhan.exams@gmail.com>
2013-10-12 09:45:22 +0530

    fixes for url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def default_url_options
+    if Rails.env.production?
+      {:host => "vijaychouhan.in"}
+    end
+  end
+
 end
